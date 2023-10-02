7faa99e82ec5ddcccefd631e276b94d70f8e6d3e
samen22 <samenjoy27@gmail.com>
2023-10-02 06:20:13 +0600

    added the contact form for mobile and desktop version both

--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -2,8 +2,53 @@ const Form = () => {
     return (
         <div className="antialiased bg-gray-100">
             <div className="flex w-full min-h-screen justify-center items-center">
-                <div className=" bg-cyan w-full max-w-4xl p-8 rounded-xl shadow-lg text-white">
-                    
+                <div className="flex flex-col md:flex-row md:space-x-6 space-y-6 md:space-y-0  bg-cyan w-full max-w-4xl p-8 rounded-xl shadow-lg text-white">
+                    <div className="flex flex-col space-y-8  justify-between ">
+                        <div>
+                            <h1 className="font-bold text-4xl tracking-wide ">Contact Us</h1>
+                            <p className="pt-2 text-neutral-200 text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, quaerat?</p>
+                        </div>
+                        <div className="flex flex-col space-y-6">
+                            <div className="inline-flex space-x-2">
+                                <img  src={`https://i.ibb.co/cbk5c0R/icons8-call-20.png`} alt="" />
+                                <span>+(123) 789 4560</span>
+                            </div>
+                            <div className="inline-flex space-x-2">
+                                <img src={`https://i.ibb.co/Y8yySPL/icons8-mail-20.png`} alt="" />
+                                <p>Contact@xyzwebsite.com</p>
+                            </div>
+                            <div className="inline-flex space-x-2">
+                                <img src={`https://i.ibb.co/7Gkt2QH/icons8-location-20.png`} alt="" />
+                                <p>11, Street 69, NYC</p>
+                            </div>
+                        </div>
+                        <div className="flex space-x-4 ">
+                            <a href=""><img src={`https://i.ibb.co/dJZh9N0/icons8-facebook-20.png`} alt="" /></a>
+                            <a href=""><img src={`https://i.ibb.co/FhdHtcz/icons8-twitter-20.png`} alt="" /></a>
+                            <a href=""><img src={`https://i.ibb.co/zZZp0JP/icons8-linkedin-20.png`} alt="" /></a>
+                            <a href=""><img src={`https://i.ibb.co/4KK4VKK/icons8-instagram-20.png`} alt="" /></a>
+                        </div>
+                    </div>
+                <div>
+                    <div className="bg-white rounded-xl shadow-lg p-8 text-gray-600">
+                        <form action="" className="flex flex-col space-y-4">
+                            <div>
+                            <label htmlFor="">Your Name</label>
+                            <input type="text" placeholder="Your name" className=" ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none focus:ring-2 focus:ring-teal" />
+                            </div>
+                            <div>
+                            <label htmlFor="">Email Address</label>
+                            <input type="text" placeholder="Email Address" className=" ring-1 ring-gray-300 w-full rounded-md px-4 mt-2 py-2 outline-none focus:ring-2 focus:ring-teal" />
+                            </div>
+                            <div>
+                            <label htmlFor="">Message</label>
+                            <textarea name="" id="" cols="" rows="5" placeholder="Message" className=" ring-1 ring-gray-300 w-full rounded-md px-4 mt-2 py-2 outline-none focus:ring-2 focus:ring-teal"></textarea>
+                            </div>
+                            <button className="inline-block self-end bg-cyan text-white font-bold rounded-lg px-6 py-2 uppercase text-sm">Send Message</button>
+
+                        </form>
+                    </div>
+                </div>
                 </div>
             </div>
         </div>
